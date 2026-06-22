664b5a04cc57d73fed9d0b4dc237f4d91fa80927
Teguhfirmansyah24 <firmansyah2448@gmail.com>
2026-06-22 07:13:55 +0700

    perkenalan

---
+++ perkenalan.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Perkenalkan Nama Saya Teguh Firmansyah Dari Kelas XI RPL 1" << std::endl;
+    std::cout << "Saya tinggal di bandung, tepatnya di rancamanyar, Indonesia" << std::endl;
+    return 0;
+}
