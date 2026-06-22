aad7644895ff89b2ea70c6f94a4169fe8bb66439
Teguhfirmansyah24 <firmansyah2448@gmail.com>
2026-06-22 08:48:25 +0700

    compiling and include

---
+++ compiling/compiling.cpp
@@ -0,0 +1,9 @@
+#include "include.h"
+
+int perkalian(int a, int b);
+
+int main()
+{
+    std::cout << perkalian(10, 4) << std::endl;
+    return 0;
+}
---
+++ compiling/perkalian.cpp
@@ -0,0 +1,4 @@
+int perkalian(int a, int b)
+{
+    return (a * b);
+}
